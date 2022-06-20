f0bccf974da7b27a50417c0cba61c98a962500a8
Darya <daryafaitelson@gmail.com>
2022-06-20 22:38:11 +0300

    добавлены циклы

--- script.js
+++ script.js
@@ -14,11 +14,26 @@ let personalMovieDatabase = {
    private: false,
 };
 
-let lastMovie1 = prompt("Один из последних просмотренных фильмов?");
-let rate1 = +prompt("На сколько его оцените?", 0);
+if (personalMovieDatabase.count > 0 && personalMovieDatabase.count < 10) {
+   alert('Просмотрено мало фильмов');
+} else if (personalMovieDatabase.count >= 10 && personalMovieDatabase.count < 30) {
+   alert('Вы классический зритель');
+} else if (personalMovieDatabase.count >= 30) {
+   alert('Вы киноман');
+} else {
+   alert('Произошла ошибка');
+}
 
-let lastMovie2 = prompt("Еще один из последних просмотренных фильмов?");
-let rate2 = +prompt("На сколько его оцените?", 0);
+for (let i = 0; i < 2; i++) {
+   let lastMovie = prompt("Один из последних просмотренных фильмов?");
+   let rate = prompt("На сколько его оцените?", 0);
+   console.log(rate);
+   if (+lastMovie !== 0 && lastMovie.length < 50 && rate !== null && rate !== '') {
+      personalMovieDatabase.movies[lastMovie] = rate;
+   } else {
+      console.log('error');
+      i--;
+   }
+}
 
-personalMovieDatabase.movies[lastMovie1] = rate1;
-personalMovieDatabase.movies[lastMovie2] = rate2;
+console.log(personalMovieDatabase);
